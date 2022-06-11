61267ee2ae176b2e8b6cd52df0309dab312de139
Benjamin Yeboah Sasu <sasuyeboahbenjamin@gmail.com>
2022-06-11 01:04:13 +0000

    organized prediction code into function

--- ML/alpha_extracter.py
+++ ML/alpha_extracter.py
@@ -2,27 +2,28 @@ import numpy as np
 import cv2
 # import matplotlib.pyplot as plt
 # from sklearn.preprocessing import StandardScaler
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 from joblib import load
 from english_words import english_words_set
 import difflib
 
+def pred_img(filename):
+  file_path = "../static/uploads/"+filename
+  image = cv2.imread(file_path)
+  im = image.copy()
+  imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+  ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+  contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-image = cv2.imread('word3.jpg')
-im = image.copy()
-imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  imgray = imgray.flatten()
 
-imgray = imgray.flatten()
+  with_contours = cv2.drawContours(im, contours, -1, (0,255,0), 3)
 
-with_contours = cv2.drawContours(im, contours, -1, (0,255,0), 3)
+  new_images = []
+  for hr,c in zip(hierarchy[0],contours):
+    x, y, w, h = cv2.boundingRect(c)
 
-new_images = []
-for hr,c in zip(hierarchy[0],contours):
-  x, y, w, h = cv2.boundingRect(c)
- 
     # Make sure contour area is large enough
   if hr[3]==0 and (cv2.contourArea(c)) > 9000:
     cv2.rectangle(with_contours,(x,y), (x+w,y+h), (255,0,0), 5)
@@ -35,28 +36,30 @@ for hr,c in zip(hierarchy[0],contours):
     print(alpha_im.shape)
     new_images.append(alpha_im)
 
-new_images = np.array(new_images)
+  new_images = np.array(new_images)
 
 
-A_Z = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-model = load_model("alpha_pred.h5")
+  A_Z = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  model = load_model("alpha_pred.h5")
 
 
-scaler=load('std_scaler.bin')
-new_images = scaler.transform(new_images)
+  scaler=load('std_scaler.bin')
+  new_images = scaler.transform(new_images)
 
-predictions = [ np.argmax(np.array(list(map(int,pred == max(pred))))) for pred in model.predict(new_images)]
-predictions = [A_Z[p] for p in predictions]
+  predictions = [ np.argmax(np.array(list(map(int,pred == max(pred))))) for pred in model.predict(new_images)]
+  predictions = [A_Z[p] for p in predictions]
 
-predicted_word = "".join(predictions)
-predicted_word = predicted_word.lower()
+  predicted_word = "".join(predictions)
+  predicted_word = predicted_word.lower()
 
-print(predicted_word)
+  print(predicted_word)
 
 
 
-if(predicted_word in english_words_set):
+  if(predicted_word in english_words_set):
     print(f"Recognised word: {predicted_word}")
-else:
+  else:
     close_matches = difflib.get_close_matches(predicted_word, english_words_set)
     print(f"Sorry we are unable to recognise your word,\nDid you mean any of these:  {close_matches}")
+  
+  return predicted_word
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, request, redirect, url_for,jsonify,flash
+from .ML import alpha_extracter
 import os
 UPLOAD_FOLDER = 'static/uploads/'
 
@@ -15,6 +16,8 @@ def upload_image():
         print("here")
         file = request.files["file"]
         file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
+        # word = alpha_extracter.pred_img(file.filename)
+        # print(word)
         data ={
             "file":file.filename,
             "message": "success"
