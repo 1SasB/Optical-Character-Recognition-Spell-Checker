f836ac547e8d6e3ab480ccb4134ff67605b0bb68
Benjamin Yeboah Sasu <sasuyeboahbenjamin@gmail.com>
2022-06-19 14:49:37 +0000

    updated requirements file and alphabet model

--- main/__init__.py
+++ main/__init__.py
@@ -7,8 +7,8 @@ from joblib import load
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secrsdf4565423{et"
 
-alpha_model = load_model('./ML/alpha_pred.h5')
-alpha_scaler = load('./ML/std_scaler.bin')
+alpha_model = load_model('./ML/alpha_pred copy.h5')
+alpha_scaler = load('./ML/std_scaler copy.bin')
 
 num_model = load_model('./ML/numeric_pred.h5')
 num_scaler = load('./ML/num_std_scaler.bin')
